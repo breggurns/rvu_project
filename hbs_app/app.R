778b293fece50e921c3aaf5085fde2cf98983d24
breggurns <97698659+breggurns@users.noreply.github.com>
2022-08-25 15:21:07 -0700

    Add files via upload

---
+++ hbs_app/app.R
@@ -0,0 +1,50 @@
+# Packages ----------------------------------------------------------------
+library(shiny)
+library(tidyverse)
+library(data.table)
+library(lubridate)
+
+# # Import Data -------------------------------------------------------------
+productive_dat <- readRDS('data/productive.rds')
+
+# Shiny App ---------------------------------------------------------------
+
+ui <- fluidPage(
+  titlePanel("HBS Report"),
+  
+  column(4, wellPanel(
+    dateRangeInput('dateRange',
+                   label = 'Filter by date',
+                   start = Sys.Date()-30, 
+                   end = Sys.Date()+30,
+    )
+  )),
+  column(4, wellPanel(
+    selectInput('campus',
+                label = 'Campus',
+                choices = c("mb", "parn")
+    )
+  )),
+  column(6,
+         dataTableOutput('my_table')
+  )
+)
+
+server <- function(input, output, session) {
+  output$my_table  <- renderDataTable({
+    
+    # Productive table, filtered by dateRange and campus
+    productive_dat %>% 
+      group_by(year, month, productive_hour_type) %>% 
+      filter(DATE >= input$dateRange[1] & 
+               DATE <= input$dateRange[2] &
+               campus == input$campus) %>% 
+      summarise(sum_hours = sum(sum_hours),
+                sum_fte = sum(sum_fte))
+    
+    # 
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
